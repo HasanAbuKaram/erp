cd2617ed9e309a9a8bdd101e83d06441ee4cc083
Hasan Yousef <abukaramhasan@gmail.com>
2024-08-24 13:02:59 +0300

    web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the ERP system!")
+}
 
 func main() {
-	fmt.Println("ERP system is up and running!")
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Starting the web server on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
